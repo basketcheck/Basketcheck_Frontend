f799f8f11f648ae7e59292b91cac11f19b92590f
Jamkris <dltmdgus1412@gmail.com>
2023-12-29 17:59:32 +0900

    ADD :: 로딩 페이지 완성

--- src/Pages/Loading/index.jsx
+++ src/Pages/Loading/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+import Logo from '../../IMG/Logo.png';
 
 const Index = () => {
-  return (
-    <div>
-      Loading Page
-    </div>
-  );
-}
+	return (
+		<div className='Loading_Container'>
+			<img src={Logo} alt='LogoIMG' className='Logo_Img' />
+			<div className='Loading_Text'>BasketBall Check</div>
+		</div>
+	);
+};
 
 export default Index;
