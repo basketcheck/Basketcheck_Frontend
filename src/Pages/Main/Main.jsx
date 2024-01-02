a0724b66251d98e6cef13e5fe5b43a480cfd0f46
ChooSeongWoo <sldoal2423@gmail.com>
2024-01-02 16:48:08 +0900

    ADD :: 참가 투표 기능 구현

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 
 import Header from "../../Components/Header/Header.jsx";
 import Logo from "../../IMG/Logo.png";
+import Vote from "../../IMG/Vote.png";
 import Button from "../../Components/UI/Button/Button.jsx";
 import { useNavigate } from "react-router-dom";
+import { customAxios } from "../../Axios/AuthAxios";
 
 const Main = () => {
   const today = new Date();
   const TodayDate = `${today.getMonth() + 1}월 ${today.getDate()}일`;
-
   const history = useNavigate();
+  const [isJoinned, setIsJoinned] = useState(false);
+
+  const onSubmit = () => {
+    if (localStorage.getItem("accessToken")) {
+      customAxios
+        .post("/team/join")
+        .then((res) => {
+          setIsJoinned((prev) => !prev);
+          console.log(res);
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    } else {
+      alert("로그인 후 이용 가능합니다.");
+      history("/auth/signin");
+    }
+  };
 
   return (
     <div>
@@ -19,22 +38,13 @@ const Main = () => {
       <div className="Main_Container">
         <div className="Main_Date">{TodayDate}</div>
         <img
-          src={Logo}
+          src={isJoinned ? Vote : Logo}
           alt="LogoIMG"
           className="Logo_Img"
-          onClick={() => {
-            if (localStorage.getItem("accessToken")) {
-            }
-          }}
+          onClick={onSubmit}
         />
-        <div
-          className="Main_GoBasketBall"
-          onClick={() => {
-            if (localStorage.getItem("accessToken")) {
-            }
-          }}
-        >
-          농구하러 가기
+        <div className="Main_GoBasketBall">
+          {isJoinned ? "취소하기" : "농구하러가기"}
         </div>
 
         <div className="Main_NowPeople">
