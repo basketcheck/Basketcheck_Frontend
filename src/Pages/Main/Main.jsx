3b25e4772905361e1486bbe5bb4638a6790ba9fb
ChooSeongWoo <sldoal2423@gmail.com>
2024-01-02 09:34:05 +0900

    CHORE :: 로그인이 돼 있으면 로그인창으로 못가게

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,51 +1,53 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
 
-import Header from '../../Components/Header/Header.jsx';
-import Logo from '../../IMG/Logo.png';
-import Button from '../../Components/UI/Button/Button.jsx';
-import { useNavigate } from 'react-router-dom';
+import Header from "../../Components/Header/Header.jsx";
+import Logo from "../../IMG/Logo.png";
+import Button from "../../Components/UI/Button/Button.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-	const today = new Date();
-	const TodayDate = `${today.getMonth() + 1}월 ${today.getDate()}일`;
+  const today = new Date();
+  const TodayDate = `${today.getMonth() + 1}월 ${today.getDate()}일`;
 
-	const history = useNavigate();
+  const history = useNavigate();
 
-	return (
-		<div>
-			<Header />
+  return (
+    <div>
+      <Header />
 
-			<div className='Main_Container'>
-				<div className='Main_Date'>{TodayDate}</div>
-				<img
-					src={Logo}
-					alt='LogoIMG'
-					className='Logo_Img'
-					onClick={() => {
-						history('/draw');
-					}}
-				/>
-				<div
-					className='Main_GoBasketBall'
-					onClick={() => {
-						history('/draw');
-					}}
-				>
-					농구하러 가기
-				</div>
+      <div className="Main_Container">
+        <div className="Main_Date">{TodayDate}</div>
+        <img
+          src={Logo}
+          alt="LogoIMG"
+          className="Logo_Img"
+          onClick={() => {
+            if (localStorage.getItem("accessToken")) history("/draw");
+            else history("/auth/signin");
+          }}
+        />
+        <div
+          className="Main_GoBasketBall"
+          onClick={() => {
+            if (localStorage.getItem("accessToken")) history("/draw");
+            else history("/auth/signin");
+          }}
+        >
+          농구하러 가기
+        </div>
 
-				<div className='Main_NowPeople'>
-					<Button
-						Text={'현재 참가자'}
-						onClickMethod={() => {
-							history('/starting');
-						}}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="Main_NowPeople">
+          <Button
+            Text={"현재 참가자"}
+            onClickMethod={() => {
+              history("/starting");
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
